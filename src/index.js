0dd057111e9ce091a1f7ae2ecefbb71ddaa2201c
fralonra <fralonra@aliyun.com>
2019-08-25 22:29:44 +0800

    init middleware

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ const server = require('./server')
 
 function felid (options = {}) {
   this.options = {
+    contentType: 'application/json',
     ...options
   }
 
+  this.middlewares = []
   this.router = router({})
 }
 
+// middleware
+felid.prototype.use = function (middle) {
+  this.middlewares.push(middle)
+}
+
+// listen
 felid.prototype.listen = function (port) {
   this.server = server({}, (req, res) => {
     this.router.lookup(req, res)
@@ -20,6 +28,7 @@ felid.prototype.listen = function (port) {
   this.port = port
 }
 
+// route
 felid.prototype.on = function (method, url, handler) {
   return this.router.on(method, url, handler)
 }
@@ -33,7 +42,7 @@ felid.prototype.delete = function (url, handler) {
 }
 
 felid.prototype.get = function (url, handler) {
-  return this.router.get(url, handler)
+  return this.router.get(url, buildHanlder.call(this, handler))
 }
 
 felid.prototype.post = function (url, handler) {
@@ -45,3 +54,22 @@ felid.prototype.put = function (url, handler) {
 }
 
 module.exports = felid
+
+function buildHanlder (handler) {
+  const middlewares = this.middlewares
+  return function (req, res) {
+    let index = 0
+    function next () {
+      if (middlewares[index]) {
+        middlewares[index++](req, res, next)
+      } else {
+        handler(req, res)
+      }
+    }
+    if (middlewares.length) {
+      next()
+    } else {
+      handler(req, res)
+    }
+  }
+}
