7cf5a7e9f7fb0f0ac3b3327f74758027b95f71b2
fralonra <fralonra@aliyun.com>
2019-08-29 23:30:17 +0800

    parse body

--- src/index.js
+++ src/index.js
@@ -62,8 +62,8 @@ module.exports = felid
 
 function buildHanlder (handler) {
   const middlewares = this.middlewares
-  return (req, res, params) => {
-    req = buildRequest(req, params)
+  return async (req, res, params) => {
+    req = await buildRequest(req, params)
     res.setHeader('content-type', this.options.contentType)
 
     let index = 0
@@ -82,7 +82,14 @@ function buildHanlder (handler) {
   }
 }
 
-function buildRequest (req, params) {
+const noBodyMethods = [
+  'get',
+  'options',
+  'head',
+  'trace'
+]
+
+async function buildRequest (req, params) {
   const queryPrefix = req.url.indexOf('?')
   if (queryPrefix >= 0) {
     req.query = querystring.parse(req.url.slice(queryPrefix + 1))
@@ -90,9 +97,33 @@ function buildRequest (req, params) {
   if (params) {
     req.params = params
   }
+  if (noBodyMethods.indexOf(req.method) >= 0) {
+    return req
+  }
+  try {
+    req.body = await buildBody(req)
+  } catch (e) {
+    req.body = null
+  }
   return req
 }
 
 function buildResponse (res) {
 
+}
+
+
+function buildBody (req) {
+  return new Promise((resolve, reject) => {
+    let body = ''
+    req.on('data', chunk => {
+      body += chunk
+    })
+    req.on('end', () => {
+      resolve(body)
+    })
+    req.on('error', err => {
+      reject(err)
+    })
+  })
 }
