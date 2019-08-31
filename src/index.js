463d9ad4cc269bafe6c998480e5a3740db1a5978
fralonra <fralonra@aliyun.com>
2019-08-31 23:30:50 +0800

    buildResponse

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function buildHanlder (handler) {
   const middlewares = this.middlewares
   return async (req, res, params) => {
     req = await buildRequest(req, params)
-    res.setHeader('content-type', this.options.contentType)
+    res = buildResponse(res)
 
     let index = 0
     function next () {
@@ -109,7 +109,26 @@ async function buildRequest (req, params) {
 }
 
 function buildResponse (res) {
-
+  res.send = (payload) => {
+    if (typeof payload === 'string') {
+      res.setHeader('content-type', 'text/plain; charset=utf-8')
+      res.end(payload)
+      return
+    }
+    if (Buffer.isBuffer(payload)) {
+      res.setHeader('content-type', 'application/octet-stream')
+      res.end(payload)
+      return
+    }
+    try {
+      const jsonPayload = JSON.stringify(payload)
+      res.setHeader('content-type', 'application/json; charset=utf-8')
+      res.end(jsonPayload)
+    } catch (e) {
+      res.end(payload)
+    }
+  }
+  return res
 }
 
 
