8fcc5263b6e2886fe2ce15f246acafa0c7c56ce5
fralonra <fralonra@aliyun.com>
2019-09-16 23:37:33 +0800

    test: http methods

--- test/app.route.test.js
+++ test/app.route.test.js
@@ -15,3 +15,31 @@ test('felid.on should send response properly', () => {
       expect(res.body).toBe('test')
     })
 })
+
+const httpMethods = [
+  'delete',
+  'get',
+  'head',
+  'options',
+  'patch',
+  'post',
+  'put',
+  'trace'
+]
+
+test('felid uses correct http method', () => {
+  const instance = new Felid()
+  
+  httpMethods.forEach(method => {
+    instance[method]('/test', (req, res) => {
+      res.send(req.method)
+    })
+    injectar(instance.router.lookup.bind(instance.router))
+      [method]('/test')
+      .end((err, res) => {
+        expect(err).toBe(null)
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toBe(method.toUpperCase())
+      })
+  })
+})
