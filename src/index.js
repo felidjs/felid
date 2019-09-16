8d202efc6f2b2103b505795a761f61cd98b86835
fralonra <fralonra@aliyun.com>
2019-09-17 00:39:13 +0800

    rewrite Felid using es6 class syntax

--- src/index.js
+++ src/index.js
@@ -3,49 +3,60 @@ const server = require('./server')
 const buildRequest = require('./request').build
 const buildResponse = require('./response').build
 
-function felid (options = {}) {
-  this.option = {
-    routeOptions: {},
-    ...options
-  }
-
-  this.middlewares = []
-  this.routeMiddlewares = {}
-  this.router = router({
-    defaultRoute: (req, res) => {
-      res.statusCode = 404
-      res.end()
-    },
-    ...this.option.routeOptions
-  })
-}
-
-// middleware
-felid.prototype.use = function (url, ...middle) {
-  if (typeof url === 'function') {
-    middle = url
-    this.middlewares.push(middle)
-  } else if (typeof url === 'string') {
-    if (!this.routeMiddlewares[url]) {
-      this.routeMiddlewares[url] = []
+class Felid {
+  constructor (options = {}) {
+    this.option = {
+      routeOptions: {},
+      ...options
     }
-    this.routeMiddlewares[url].push(...middle)
-  } else if (Array.isArray(url)) {
-    url.forEach(path => {
-      this.use(path, ...middle)
+  
+    this.middlewares = []
+    this.routeMiddlewares = {}
+    this.router = router({
+      defaultRoute: (req, res) => {
+        res.statusCode = 404
+        res.end()
+      },
+      ...this.option.routeOptions
     })
   }
-}
 
-// listen
-felid.prototype.listen = function (port, callback) {
-  const options = typeof port === 'number' ? { port } : port
-  this.port = options.port
+  // middleware
+  use (url, ...middle) {
+    if (typeof url === 'function') {
+      middle = url
+      this.middlewares.push(middle)
+    } else if (typeof url === 'string') {
+      if (!this.routeMiddlewares[url]) {
+        this.routeMiddlewares[url] = []
+      }
+      this.routeMiddlewares[url].push(...middle)
+    } else if (Array.isArray(url)) {
+      url.forEach(path => {
+        this.use(path, ...middle)
+      })
+    }
+  }
 
-  this.server = server((req, res) => {
-    this.router.lookup(req, res)
-  })
-  this.server.listen(options, callback)
+  // listen
+  listen (port, callback) {
+    const options = typeof port === 'number' ? { port } : port
+    this.port = options.port
+  
+    this.server = server((req, res) => {
+      this.router.lookup(req, res)
+    })
+    this.server.listen(options, callback)
+  }
+
+  // route
+  on (method, url, handler) {
+    return this.router.on(method.toUpperCase(), url, buildHanlder.call(this, url, handler))
+  }
+
+  all (url, handler, store) {
+    return this.router.all(url, buildHanlder.call(this, url, handler), store)
+  }
 }
 
 const httpMethods = [
@@ -59,22 +70,13 @@ const httpMethods = [
   'trace'
 ]
 
-// route
-felid.prototype.on = function (method, url, handler) {
-  return this.router.on(method.toUpperCase(), url, buildHanlder.call(this, url, handler))
-}
-
-felid.prototype.all = function (url, handler, store) {
-  return this.router.all(url, buildHanlder.call(this, url, handler), store)
-}
-
 httpMethods.forEach(method => {
-  felid.prototype[method] = function (url, handler) {
+  Felid.prototype[method] = function (url, handler) {
     return this.router[method](url, buildHanlder.call(this, url, handler))
   }
 })
 
-module.exports = felid
+module.exports = Felid
 
 function buildHanlder (url, handler) {
   const middlewares = this.middlewares.concat(this.routeMiddlewares[url] || [])
@@ -97,8 +99,3 @@ function buildHanlder (url, handler) {
     }
   }
 }
-
-// TODO: split handler, req, res
-// TODO: parse body
-// TODO: route middlewares
-// TODO: hooks: handle error...
