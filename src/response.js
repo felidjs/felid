1e0d1ae1de6322539876b98cd86e3a6b848bc98d
fralonra <fralonra@aliyun.com>
2019-09-17 01:03:02 +0800

    response.js: bind functions to response

--- src/response.js
+++ src/response.js
@@ -1,35 +1,41 @@
+function code (code) {
+  this.statusCode = code
+  return this
+}
+
+function redirect (code, url) {
+  if (typeof code === 'string') {
+    url = code
+    code = 302
+  }
+  this.setHeader('location', url)
+  this.code(code).send()
+}
+
+function send (payload) {
+  if (typeof payload === 'string') {
+    this.setHeader('content-type', 'text/plain; charset=utf-8')
+    this.end(payload)
+    return
+  }
+  if (Buffer.isBuffer(payload)) {
+    this.setHeader('content-type', 'application/octet-stream')
+    this.end(payload)
+    return
+  }
+  try {
+    const jsonPayload = JSON.stringify(payload)
+    this.setHeader('content-type', 'application/json; charset=utf-8')
+    this.end(jsonPayload)
+  } catch (e) {
+    this.end(payload)
+  }
+}
+
 function buildResponse (res) {
-  res.code = (code) => {
-    res.statusCode = code
-    return res
-  }
-  res.redirect = (code, url) => {
-    if (typeof code === 'string') {
-      url = code
-      code = 302
-    }
-    res.setHeader('location', url)
-    res.code(code).send()
-  }
-  res.send = (payload) => {
-    if (typeof payload === 'string') {
-      res.setHeader('content-type', 'text/plain; charset=utf-8')
-      res.end(payload)
-      return
-    }
-    if (Buffer.isBuffer(payload)) {
-      res.setHeader('content-type', 'application/octet-stream')
-      res.end(payload)
-      return
-    }
-    try {
-      const jsonPayload = JSON.stringify(payload)
-      res.setHeader('content-type', 'application/json; charset=utf-8')
-      res.end(jsonPayload)
-    } catch (e) {
-      res.end(payload)
-    }
-  }
+  res.redirect = code.bind(res)
+  res.redirect = redirect.bind(res)
+  res.send = send.bind(res)
   return res
 }
 
