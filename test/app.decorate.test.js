da45fd191576eda0a9c44316d7b664c9b56af700
fralonra <fralonra@aliyun.com>
2019-10-23 12:20:12 +0800

    test: app.decorate.test.js

---
+++ test/app.decorate.test.js
@@ -0,0 +1,92 @@
+const Felid = require('../src')
+
+// decorate
+test('felid.decorate should add new property to the Felid instance', () => {
+  const instance = new Felid()
+  instance.decorate('key', 'value')
+  instance.decorate('key2', 'value2')
+
+  expect(instance.key).toBe('value')
+  expect(instance.key2).toBe('value2')
+})
+
+test('felid.decorate should throw if there is undefined argument', () => {
+  const instance = new Felid()
+  expect(() => {
+    instance.decorate()
+  }).toThrow()
+  expect(() => {
+    instance.decorate('key')
+  }).toThrow()
+})
+
+test('felid.decorate should throw if the Felid instance already has a property', () => {
+  const instance = new Felid()
+  instance.decorate('key', 'value')
+
+  expect(() => {
+    instance.decorate('key', 'another value')
+  }).toThrow()
+})
+
+// decorateRequest
+test('felid.decorateRequest should add new property to the Request object', () => {
+  const instance = new Felid()
+  instance.decorateRequest('key', 'value')
+  instance.decorateRequest('key2', 'value2')
+
+  instance.on('get', '/test', (req, res) => {
+    expect(req.key).toBe('value')
+    expect(req.key2).toBe('value2')
+  })
+})
+
+test('felid.decorateRequest should throw if there is undefined argument', () => {
+  const instance = new Felid()
+  expect(() => {
+    instance.decorateRequest()
+  }).toThrow()
+  expect(() => {
+    instance.decorateRequest('key')
+  }).toThrow()
+})
+
+test('felid.decorateRequest should throw if the Request object already has a property', () => {
+  const instance = new Felid()
+  instance.decorateRequest('key', 'value')
+
+  expect(() => {
+    instance.decorateRequest('key', 'value2')
+  }).toThrow()
+})
+
+// decorateResponse
+test('felid.decorateResponse should add new property to the Response object', () => {
+  const instance = new Felid()
+  instance.decorateResponse('key', 'value')
+  instance.decorateResponse('key2', 'value2')
+
+  instance.on('get', '/test', (req, res) => {
+    expect(res.key).toBe('value')
+    expect(res.key2).toBe('value2')
+  })
+})
+
+test('felid.decorateResponse should throw if there is undefined argument', () => {
+  const instance = new Felid()
+  expect(() => {
+    instance.decorateResponse()
+  }).toThrow()
+  expect(() => {
+    instance.decorateResponse('key')
+  }).toThrow()
+})
+
+test('felid.decorateResponse should throw if the Response object already has a property', () => {
+  const instance = new Felid()
+  instance.decorateResponse('key', 'value')
+
+  expect(() => {
+    instance.decorateResponse('key', 'another value')
+  }).toThrow()
+})
