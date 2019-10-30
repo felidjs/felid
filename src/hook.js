4d007ba0000939229cbab9a2d1e45216ca1aa727
fralonra <fralonra@aliyun.com>
2019-10-30 13:38:38 +0800

    hooks.js: refactor: class syntax

--- src/hook.js
+++ src/hook.js
@@ -11,61 +11,60 @@ const availableHooks = [
   HOOK_POST_RESPONSE
 ]
 
-function Hooks () {
-  this.hookMap = new Map()
-  this.routeHookMap = new Map()
-  this.cache = new Map()
-  availableHooks.forEach(hookName => {
-    this.cache.set(hookName, new Map())
-  })
-}
-
-Hooks.prototype.add = function (hookName, url, ...handlers) {
-  if (typeof url === 'function') {
-    handlers = [url, ...handlers]
-    url = undefined
-  }
-  assert.ok(availableHooks.includes(hookName), `Invalid hook: ${hookName}`)
-  if (url === undefined) {
-    addHandlers(this.hookMap, hookName, handlers)
-  } else if (typeof url === 'string') {
-    if (!this.routeHookMap.has(url)) {
-      this.routeHookMap.set(url, new Map())
-    }
-    addHandlers(this.routeHookMap.get(url), hookName, handlers)
-  } else {
-    assert.ok(Array.isArray(url), 'Url attached to the hook must be a string or an array of string')
-    url.forEach(path => {
-      assert.strictEqual(typeof path, 'string', 'Url in array attached to the hook must be a string')
-      this.add(hookName, path, ...handlers)
+class Hooks {
+  constructor () {
+    this.hookMap = new Map()
+    this.routeHookMap = new Map()
+    this.cache = new Map()
+    availableHooks.forEach(hookName => {
+      this.cache.set(hookName, new Map())
     })
   }
-}
 
-Hooks.prototype.get = function (hookName, url) {
-  const hookCache = this.cache.get(hookName)
-  function endHooks () {
-    hookCache.set(url, [])
-    return []
+  add (hookName, url, ...handlers) {
+    if (typeof url === 'function') {
+      handlers = [url, ...handlers]
+      url = undefined
+    }
+    assert.ok(availableHooks.includes(hookName), `Invalid hook: ${hookName}`)
+    if (url === undefined) {
+      addHandlers(this.hookMap, hookName, handlers)
+    } else if (typeof url === 'string') {
+      if (!this.routeHookMap.has(url)) {
+        this.routeHookMap.set(url, new Map())
+      }
+      addHandlers(this.routeHookMap.get(url), hookName, handlers)
+    } else {
+      assert.ok(Array.isArray(url), 'Url attached to the hook must be a string or an array of string')
+      url.forEach(path => {
+        assert.strictEqual(typeof path, 'string', 'Url in array attached to the hook must be a string')
+        this.add(hookName, path, ...handlers)
+      })
+    }
   }
-
-  if (hookCache.has(url)) {
-    return hookCache.get(url)
+  
+  get (hookName, url) {
+    const hookCache = this.cache.get(hookName)
+    function endHooks () {
+      hookCache.set(url, [])
+      return []
+    }
+  
+    if (hookCache.has(url)) {
+      return hookCache.get(url)
+    }
+    if (!this.hookMap.has(hookName) && !this.routeHookMap.has(url)) {
+      return endHooks()
+    }
+    const fns = concatHooks(this.hookMap, this.routeHookMap, hookName, url)
+    hookCache.set(url, fns)
+    return fns
   }
-  if (!this.hookMap.has(hookName) && !this.routeHookMap.has(url)) {
-    return endHooks()
+  
+  run (hookName, url, ...args) {
+    const fns = this.get(hookName, url)
+    return runHooks(fns, ...args)
   }
-  const fns = concatHooks(this.hookMap, this.routeHookMap, hookName, url)
-  if (!fns || !fns.length) {
-    return endHooks()
-  }
-  hookCache.set(url, fns)
-  return fns
-}
-
-Hooks.prototype.run = function (hookName, url, ...args) {
-  const fns = this.get(hookName, url)
-  return runHooks(fns, ...args)
 }
 
 module.exports = Hooks
