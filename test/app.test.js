5177f251de52133eca0b50b0e0d446bbf62ec000
fralonra <fralonra@aliyun.com>
2019-10-30 09:26:57 +0800

    test: error handle

--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,4 @@
+const injectar = require('injectar')
 const Felid = require('../src')
 
 test('felid instance should be an object', () => {
@@ -12,4 +13,41 @@ test('felid.listen() should listen on the given port', () => {
   instance.listen(3000, () => {
     expect(instance.address.port).toBe(3000)
   })
+})
+
+// error handle
+test('handle error thrown by felid.preRequest()', () => {
+  const instance = new Felid()
+  instance.preRequest((req, res) => {
+    throw new Error('Boom!')
+  })
+  instance.get('/test', (req, res) => {
+    res.send('test')
+  })
+
+  injectar(instance.lookup())
+    .get('/test')
+    .end((err, res) => {
+      expect(err).toBe(null)
+      expect(res.statusCode).toBe(500)
+      expect(res.payload).toBe('Boom!')
+    })
+})
+
+test('handle error thrown by felid.use()', () => {
+  const instance = new Felid()
+  instance.use((req, res) => {
+    throw new Error('Boom!')
+  })
+  instance.get('/test', (req, res) => {
+    res.send('test')
+  })
+
+  injectar(instance.lookup())
+    .get('/test')
+    .end((err, res) => {
+      expect(err).toBe(null)
+      expect(res.statusCode).toBe(500)
+      expect(res.payload).toBe('Boom!')
+    })
 })
