2145d550209ff948dc960296a389ea16dc4dfafa
fralonra <fralonra@aliyun.com>
2019-11-18 00:13:36 +0800

    chore: package.json: add jest coverage options

---
+++ examples/hooks.js
@@ -0,0 +1,23 @@
+const Felid = require('../src')
+
+const app = new Felid()
+
+app.preRequest((req, res) => {
+  console.log('before every route')
+})
+
+app.use((req, res) => {
+  console.log('every route goes here')
+})
+
+app.postResponse((req, res) => {
+  console.log('after every request has been responded')
+})
+
+app.get('/', (req, res) => {
+  res.send('hello felid')
+})
+
+app.listen(8080, () => {
+  console.log(`felid server listen on ${app.address.port}`)
+})
