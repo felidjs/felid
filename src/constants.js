76d8e5747da733a3ed3a4a53c05b03af33ab9f96
fralonra <fralonra@aliyun.com>
2020-01-22 14:47:12 +0800

    chore: remove src/ test/

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // hooks
-  HOOK_PRE_REQUEST: 'preRequest',
-  HOOK_MIDDLE: 'middle',
-  HOOK_POST_RESPONSE: 'postResponse'
-}
--- src/router.js
+++
@@ -1,7 +0,0 @@
-const findMyWay = require('find-my-way')
-
-function router (options) {
-  return findMyWay(options)
-}
-
-module.exports = router
